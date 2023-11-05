878d7d3c674bcb728bb185a1bc2168bacaeae3b5
kust <vad.velikanov@yandex.ru>
2023-11-05 17:19:27 +0300

    Add project files.

---
+++ Ex_1.3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+int[,] matrix = new int[4, 4];
+int n = 4;
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+int res = 0;
+for (int i = 0; i < n; i++)
+{
+    int j = i;
+    res += matrix[i, j];
+}
+Console.WriteLine(res);
