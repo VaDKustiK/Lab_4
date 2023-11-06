3526f3838bc41191ad042b2442d96c9b0b0af3ae
kust <vad.velikanov@yandex.ru>
2023-11-06 20:31:47 +0300

    exercise 6 (level 1)

---
+++ Ex_1.6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+int[] array = new int[4];
+int[,] A = new int[4, 7];
+Console.WriteLine("Введите значения матрицы");
+for (int i = 0; i < 4; i++)
+{
+    for (int j = 0; j < 7; j++)
+    {
+        A[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+for (int i = 0; i < 4; i++)
+{
+    int min1 = int.MaxValue;
+    for (int j = 0; j < 7; j++)
+    {
+        if (A[i, j] < min1)
+        {
+            min1 = A[i, j];
+            array[i] = j;
+        }
+    }
+}
+Console.WriteLine(string.Join(", ", array));
