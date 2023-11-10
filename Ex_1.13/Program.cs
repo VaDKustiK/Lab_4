b7eb7b682e6d904cad9134cf9567fd7f2ff0e5c8
kust <vad.velikanov@yandex.ru>
2023-11-10 17:08:24 +0300

    exercise 13 (lvl 1)

---
+++ Ex_1.13/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+int n = 5, m = 5;
+int[,] A = new int[n, m];
+
+Console.WriteLine("Введите значения матрицы");
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        A[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+int J = 0;
+int max1 = int.MinValue;
+for (int i = 0; i < n; i++)
+{
+    int j = i;
+    if (A[i, j] > max1)
+    {
+        max1 = A[i, j];
+        J = j;
+    }
+}
+
+int temp = 0;
+for (int i = 0; i < n; i++)
+{
+    temp = A[i, J];
+    A[i, J] = A[i, 3];
+    A[i, 3] = temp;
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        Console.Write(A[i, j] + " ");
+    }
+    Console.WriteLine();
+}
