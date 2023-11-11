3c38b245f678ddc1c9eeb918ade534095637cf8e
kust <vad.velikanov@yandex.ru>
2023-11-11 14:37:02 +0300

    exercise 17 (lvl 1)

---
+++ Ex_1.17/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine("Введите значение n");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение m");
+int m = int.Parse(Console.ReadLine());
+
+int[,] B = new int[n, m];
+
+Console.WriteLine("Введите значения матрицы");
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        B[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+for (int i = 0; i < n; i++)
+{
+    int mini = 0;
+    int minelem = B[i, mini];
+    for (int j = 1; j < m; j++)
+    {
+        if (B[i, j] < minelem)
+        {
+            mini = j;
+            minelem = B[i, mini];
+        }
+    }
+    for (int k = mini; k > 0; k--)
+    {
+        int temp = B[i, k];
+        B[i, k] = B[i, k - 1];
+        B[i, k - 1] = temp;
+    }
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        Console.Write(B[i, j] + " ");
+    }
+    Console.WriteLine();
+}
