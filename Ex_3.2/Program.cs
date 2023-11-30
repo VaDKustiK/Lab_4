6e9ea212877e89b98ebfc313d31ea895e88708f9
kust <vad.velikanov@yandex.ru>
2023-11-30 22:12:47 +0300

    exercise 2 (lvl 3)

---
+++ Ex_3.2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine("Введите количество строк");
+int n = int.Parse(Console.ReadLine());
+int[,] A = new int[n, n];
+Random r = new Random();
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        A[i, j] = r.Next(9);
+    }
+}
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(A[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (i == 0 || j == 0 || i == n-1 || j == n-1)
+            A[i, j] = 0;
+    }
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(A[i, j] + " ");
+    }
+    Console.WriteLine();
+}
