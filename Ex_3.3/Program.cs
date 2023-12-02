1299bc3b6146bc5e93b0681e325804a4976c40ce
kust <vad.velikanov@yandex.ru>
2023-12-02 17:07:16 +0300

    exercise 3 (lvl 3)

---
+++ Ex_3.3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine("Введите значение N");
+int n = int.Parse(Console.ReadLine());
+int[,] A = new int[n, n];
+Random r = new Random();
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        A[i, j] = r.Next(10);
+    }
+}
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(A[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+int[] b = new int[2*n - 1];
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        int ind = j - i + n - 1;
+        b[ind] += A[i, j];
+    }
+}
+
+Console.WriteLine("[{0}]", string.Join(", ", b));
