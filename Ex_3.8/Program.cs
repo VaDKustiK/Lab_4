cf42eadde5082849164bb489fb9a2fd5e6768c35
kust <vad.velikanov@yandex.ru>
2023-12-08 17:32:53 +0300

    exercise 8 (lvl 3)

---
+++ Ex_3.8/Program.cs
@@ -0,0 +1,51 @@
+﻿int n = 7, m = 5;
+int[,] A = new int[7, 5] { {-5, -4, -3, -2, -1},
+                                 {-4, -3, -2, -1, 0},
+                                 {-3, -2, -1, 0, 1},
+                                 {-2, -1, 0, 1, 2},
+                                 {-1, 0, 1, 2, 3},
+                                 {0, 1, 2, 3, 4},
+                                 {1, 2, 3, 4, 5} };
+int[] s = new int[n];
+int[] ind = new int[n];
+for (int i = 0; i < n; i++)
+{
+    int count = 0;
+    for (int j = 0; j < m; j++)
+    {
+        if (A[i, j] >= 0)
+        {
+            count++;
+        }
+    }
+    s[i] = count;
+    ind[i] = i;
+}
+for (int i = 0; i < n-1; i++)
+{
+    int maxi = s[i];
+    int maxind = i;
+    for (int j = i + 1; j < 7; j++)
+    {
+        if (s[j] > maxi)
+        {
+            maxi = s[j];
+            maxind = j;
+        }
+    }
+    s[maxind] = s[i];
+    s[i] = maxi;
+    int temp = ind[maxind];
+    ind[maxind] = ind[i];
+    ind[i] = temp;
+}
+
+for (int i = 0; i < n; i++)
+{
+    int temp = ind[i];
+    for (int j = 0; j < m; j++)
+    {
+        Console.Write(A[temp, j] + " ");
+    }
+    Console.WriteLine();
+}
